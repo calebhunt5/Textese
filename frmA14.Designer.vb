dedcd7f2307692925e7637595c5a428c8094f11c
Caleb Hunter <64340885+calebhunt5@users.noreply.github.com>
2021-01-23 22:55:30 -0500

    Add files via upload

---
+++ frmA14.Designer.vb
@@ -0,0 +1,163 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmA14
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.grpConversion = New System.Windows.Forms.GroupBox()
+        Me.btnToggleDictionary = New System.Windows.Forms.Button()
+        Me.btnTranslate = New System.Windows.Forms.Button()
+        Me.txtTextese = New System.Windows.Forms.TextBox()
+        Me.lblTextese = New System.Windows.Forms.Label()
+        Me.txtEnglish = New System.Windows.Forms.TextBox()
+        Me.lblEnglish = New System.Windows.Forms.Label()
+        Me.grpDictionary = New System.Windows.Forms.GroupBox()
+        Me.dgvDictionary = New System.Windows.Forms.DataGridView()
+        Me.grpConversion.SuspendLayout()
+        Me.grpDictionary.SuspendLayout()
+        CType(Me.dgvDictionary, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'grpConversion
+        '
+        Me.grpConversion.Controls.Add(Me.btnToggleDictionary)
+        Me.grpConversion.Controls.Add(Me.btnTranslate)
+        Me.grpConversion.Controls.Add(Me.txtTextese)
+        Me.grpConversion.Controls.Add(Me.lblTextese)
+        Me.grpConversion.Controls.Add(Me.txtEnglish)
+        Me.grpConversion.Controls.Add(Me.lblEnglish)
+        Me.grpConversion.Font = New System.Drawing.Font("Ebrima", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.grpConversion.Location = New System.Drawing.Point(12, 12)
+        Me.grpConversion.Name = "grpConversion"
+        Me.grpConversion.Size = New System.Drawing.Size(582, 244)
+        Me.grpConversion.TabIndex = 0
+        Me.grpConversion.TabStop = False
+        Me.grpConversion.Text = "Conversion Station"
+        '
+        'btnToggleDictionary
+        '
+        Me.btnToggleDictionary.Font = New System.Drawing.Font("Ebrima", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnToggleDictionary.Location = New System.Drawing.Point(10, 191)
+        Me.btnToggleDictionary.Name = "btnToggleDictionary"
+        Me.btnToggleDictionary.Size = New System.Drawing.Size(250, 31)
+        Me.btnToggleDictionary.TabIndex = 4
+        Me.btnToggleDictionary.Text = "Toggle Dictionary" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        Me.btnToggleDictionary.UseVisualStyleBackColor = True
+        '
+        'btnTranslate
+        '
+        Me.btnTranslate.Font = New System.Drawing.Font("Ebrima", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnTranslate.Location = New System.Drawing.Point(10, 94)
+        Me.btnTranslate.Name = "btnTranslate"
+        Me.btnTranslate.Size = New System.Drawing.Size(250, 31)
+        Me.btnTranslate.TabIndex = 1
+        Me.btnTranslate.Text = "Translate to Textese"
+        Me.btnTranslate.UseVisualStyleBackColor = True
+        '
+        'txtTextese
+        '
+        Me.txtTextese.Enabled = False
+        Me.txtTextese.Font = New System.Drawing.Font("Ebrima", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtTextese.Location = New System.Drawing.Point(10, 154)
+        Me.txtTextese.Name = "txtTextese"
+        Me.txtTextese.Size = New System.Drawing.Size(544, 31)
+        Me.txtTextese.TabIndex = 3
+        '
+        'lblTextese
+        '
+        Me.lblTextese.AutoSize = True
+        Me.lblTextese.Font = New System.Drawing.Font("Ebrima", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTextese.Location = New System.Drawing.Point(6, 128)
+        Me.lblTextese.Name = "lblTextese"
+        Me.lblTextese.Size = New System.Drawing.Size(71, 23)
+        Me.lblTextese.TabIndex = 2
+        Me.lblTextese.Text = "Textese:"
+        '
+        'txtEnglish
+        '
+        Me.txtEnglish.Font = New System.Drawing.Font("Ebrima", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtEnglish.Location = New System.Drawing.Point(10, 57)
+        Me.txtEnglish.Name = "txtEnglish"
+        Me.txtEnglish.Size = New System.Drawing.Size(544, 31)
+        Me.txtEnglish.TabIndex = 1
+        '
+        'lblEnglish
+        '
+        Me.lblEnglish.AutoSize = True
+        Me.lblEnglish.Font = New System.Drawing.Font("Ebrima", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblEnglish.Location = New System.Drawing.Point(6, 31)
+        Me.lblEnglish.Name = "lblEnglish"
+        Me.lblEnglish.Size = New System.Drawing.Size(68, 23)
+        Me.lblEnglish.TabIndex = 0
+        Me.lblEnglish.Text = "English:"
+        '
+        'grpDictionary
+        '
+        Me.grpDictionary.Controls.Add(Me.dgvDictionary)
+        Me.grpDictionary.Font = New System.Drawing.Font("Ebrima", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.grpDictionary.Location = New System.Drawing.Point(600, 12)
+        Me.grpDictionary.Name = "grpDictionary"
+        Me.grpDictionary.Size = New System.Drawing.Size(363, 366)
+        Me.grpDictionary.TabIndex = 1
+        Me.grpDictionary.TabStop = False
+        Me.grpDictionary.Text = "English to Textese Dictionary"
+        '
+        'dgvDictionary
+        '
+        Me.dgvDictionary.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvDictionary.Location = New System.Drawing.Point(6, 34)
+        Me.dgvDictionary.Name = "dgvDictionary"
+        Me.dgvDictionary.RowHeadersWidth = 51
+        Me.dgvDictionary.RowTemplate.Height = 24
+        Me.dgvDictionary.Size = New System.Drawing.Size(345, 318)
+        Me.dgvDictionary.TabIndex = 0
+        '
+        'frmA14
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(975, 385)
+        Me.Controls.Add(Me.grpDictionary)
+        Me.Controls.Add(Me.grpConversion)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "frmA14"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Assignment 14 - Textese"
+        Me.grpConversion.ResumeLayout(False)
+        Me.grpConversion.PerformLayout()
+        Me.grpDictionary.ResumeLayout(False)
+        CType(Me.dgvDictionary, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents grpConversion As GroupBox
+    Friend WithEvents btnTranslate As Button
+    Friend WithEvents txtTextese As TextBox
+    Friend WithEvents lblTextese As Label
+    Friend WithEvents txtEnglish As TextBox
+    Friend WithEvents lblEnglish As Label
+    Friend WithEvents btnToggleDictionary As Button
+    Friend WithEvents grpDictionary As GroupBox
+    Friend WithEvents dgvDictionary As DataGridView
+End Class
---
+++ frmA14.vb
@@ -0,0 +1,130 @@
+﻿Public Class frmA14
+    'Creation of constants to locate indexes of specific values
+    Const IDX_MAX_TRANSLATION As Integer = 1
+    Const IDX_MAX_WORDS As Integer = 68
+    Const IDX_WORD As Integer = 0
+    Const IDX_ABRV As Integer = 1
+
+    'Creation of valriables to hold file values and translations
+    Dim dicShowing As Boolean = False
+    Dim dictionaryStrings(IDX_MAX_TRANSLATION, 0) As String
+    Dim dictionaryTranslations(IDX_MAX_WORDS) As Translation
+    Dim idxWord As Integer = 0
+
+    'structure that holds an english word and the abbreviation for that word
+    Structure Translation
+        Dim word As String
+        Dim abrv As String
+    End Structure
+
+    'form load that sizes the form creates defaults for dictionaryTranslations and fills the user's translation dictionary
+    Private Sub frmA14_Load(sender As Object, e As EventArgs) Handles Me.Load
+        grpDictionary.Visible = False
+        Size = New Size(490, 317)
+
+        dictionaryTranslations(0).word = "DEFAULT WORD"
+        dictionaryTranslations(0).abrv = "DEFAULT ABRV"
+        initializeDictionaryTranslations()
+        fillDictionary()
+    End Sub
+
+    'takes dictionary in or out of form
+    Private Sub btnShowDictionary_Click(sender As Object, e As EventArgs) Handles btnToggleDictionary.Click
+        toggleDictionary()
+    End Sub
+
+    'translates inputed english sentence and outputs textese sentence
+    Private Sub btnTranslate_Click(sender As Object, e As EventArgs) Handles btnTranslate.Click
+        'creates arrays for two languages and a string to output the translated sentence
+        Dim englishSentence() As String = txtEnglish.Text.ToLower.Split(" "c)
+        Dim texteseSentence(englishSentence.GetUpperBound(0)) As String
+        Dim outputSentence As String
+
+        For i As Integer = 0 To texteseSentence.GetUpperBound(0)
+            texteseSentence(i) = englishSentence(i)
+        Next
+
+        'swaps out words that can be abbreviated
+        For i As Integer = 0 To englishSentence.GetUpperBound(0)
+            For j As Integer = 0 To dictionaryTranslations.GetUpperBound(0)
+                If englishSentence(i) = dictionaryTranslations(j).word Then
+                    texteseSentence(i) = dictionaryTranslations(j).abrv
+                End If
+            Next
+        Next
+
+        'turns array into a string for output
+        For Each word In texteseSentence
+            outputSentence = outputSentence & Space(1) & word
+        Next
+
+        'outputed translated sentence
+        txtTextese.Text = outputSentence
+    End Sub
+
+    'fills dgv with words and their abbreviations
+    Sub fillDictionary()
+        Dim translateQuery = From translation In dictionaryTranslations
+                             Order By translation.word Ascending
+                             Select translation.word, translation.abrv
+
+        dgvDictionary.DataSource = translateQuery.ToList
+        dgvDictionary.CurrentCell = Nothing
+        dgvDictionary.RowHeadersVisible = False
+
+        dgvDictionary.Columns("word").HeaderText = "Full Word"
+        dgvDictionary.Columns("abrv").HeaderText = "Abbreviation"
+
+        dgvDictionary.AutoResizeColumns()
+        dgvDictionary.AutoResizeRows()
+
+    End Sub
+
+    'gives value to array that holds translations
+    Sub initializeDictionaryTranslations()
+        initializeDictionaryStrings()
+
+        For i As Integer = 0 To IDX_MAX_WORDS
+            dictionaryTranslations(i).word = dictionaryStrings(IDX_WORD, i)
+            dictionaryTranslations(i).abrv = dictionaryStrings(IDX_ABRV, i)
+        Next
+    End Sub
+
+    'gives value to array that holds translations in strings instead of the translation structure
+    Sub initializeDictionaryStrings()
+        Dim allWords() As String = IO.File.ReadAllLines("Textese-1.txt")
+
+        Dim line As String
+        Dim data() As String
+
+        For i As Integer = 0 To allWords.GetUpperBound(0)
+            idxWord = i
+
+            ReDim Preserve dictionaryStrings(IDX_MAX_TRANSLATION, idxWord)
+
+            line = allWords(i)
+            data = line.Split(","c)
+
+            For j As Integer = 0 To data.GetUpperBound(0)
+                dictionaryStrings(j, i) = data(j)
+            Next j
+        Next i
+    End Sub
+
+    'changes form size and makes dictionary visible or not visible
+    Sub toggleDictionary()
+        If dicShowing Then
+            dicShowing = False
+
+            grpDictionary.Visible = False
+
+            Size = New Size(490, 317)
+        Else
+            dicShowing = True
+
+            grpDictionary.Visible = True
+
+            Size = New Size(775, 375)
+        End If
+    End Sub
+End Class
